f60bf0280dfbd40e17b38463ea64bdd5488a47b7
Seongmo Jeon <oeeen3@gmail.com>
2019-11-26 16:05:20 +0900

    initial commit

---
+++ src/main/java/com/olbimacoojam/heaven/HeavenApplication.java
@@ -0,0 +1,13 @@
+package com.olbimacoojam.heaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/olbimacoojam/heaven/HeavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olbimacoojam.heaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
