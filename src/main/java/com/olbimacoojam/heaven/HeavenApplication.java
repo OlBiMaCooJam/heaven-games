9bf120d61ed82cc076de94f5b1726ed4b521edb3
seokhyeonchoi <lkamna12@gmail.com>
2019-12-01 18:39:11 +0900

    style: formatting

--- src/main/java/com/olbimacoojam/heaven/HeavenApplication.java
+++ src/main/java/com/olbimacoojam/heaven/HeavenApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HeavenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HeavenApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HeavenApplication.class, args);
+    }
 
 }
