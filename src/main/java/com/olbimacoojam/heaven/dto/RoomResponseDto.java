f2ec9cf212613ef8b403d3e03b4af73445195dbb
KIMJUNGMIN <kimjungmin.developer@gmail.com>
2019-12-04 19:17:47 +0900

    refactor: lombok 적용

--- src/main/java/com/olbimacoojam/heaven/dto/RoomResponseDto.java
+++ src/main/java/com/olbimacoojam/heaven/dto/RoomResponseDto.java
@@ -1,13 +1,16 @@
 package com.olbimacoojam.heaven.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class RoomResponseDto {
     private int id;
 
     public RoomResponseDto(int id) {
         this.id = id;
-    }
-
-    public int getId() {
-        return id;
     }
 }
