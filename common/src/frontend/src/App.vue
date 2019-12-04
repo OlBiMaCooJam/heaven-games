0e79581f6623eb5b0901fb137b265fb89dee74b0
SEOKHYOENCHOI <42906668+SEOKHYOENCHOI@users.noreply.github.com>
2019-12-04 17:19:51 +0900

    feat: 게임 리스트 #3 (#10)
    * style: formatting
    
    * feat: game list 페이지 구현
    
    * feat: 홈 버튼 및 title 추가
    
    * feat: 데이터구조 변경
    
    * feat: 게임 클릭 시 해당 게임으로 이동
    
    * style: 게임이름 오타 수정

---
+++ common/src/frontend/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+    <v-app id="app">
+        <div id="nav">
+            <router-link id="home" to="/">
+                <img id="btn-home" src="./assets/Logo.jpg">
+            </router-link>
+            <span id="title">미 니 게 임 천 국</span>
+        </div>
+        <router-view/>
+    </v-app>
+</template>
+
+<style lang="scss">
+    @font-face {
+        font-family: 'rose';
+        src: url('assets/a장미다방.otf')
+    }
+
+    #app {
+        font-family: 'rose';
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        background-image: url('assets/Background.png');
+        height: 100%;
+    }
+
+    #nav {
+        padding: 30px;
+    }
+
+    #btn-home {
+        width: 50px;
+    }
+
+    #home {
+        display: unset;
+        float: left;
+    }
+
+    #title {
+        font-size: 5em;
+        line-height: 1;
+        display: inline-block;
+        margin-left: -50px;
+    }
+</style>
