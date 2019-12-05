951f04b662aee6c655a0aa5cdbaea1b0d6e3476a
hyogeon <men1219@hanmail.net>
2019-12-05 17:22:02 +0900

    feat: UserEntity Column 추가

--- src/main/java/com/olbimacoojam/heaven/game/User.java
+++ src/main/java/com/olbimacoojam/heaven/game/User.java
@@ -2,8 +2,9 @@ package com.olbimacoojam.heaven.game;
 
 import com.olbimacoojam.heaven.BaseEntity;
 import lombok.Getter;
+import lombok.ToString;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 /**
  * temporary class
@@ -11,5 +12,20 @@ import javax.persistence.Entity;
 
 @Getter
 @Entity
+@ToString
 public class User extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private Long kakaoId;
+
+    @Column
+    private String name;
+
+    public User(Long kakaoId, String name) {
+        this.kakaoId = kakaoId;
+        this.name = name;
+    }
 }
