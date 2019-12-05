3b16cb97a7754da30706549c4620f4325b08aedc
seokhyeonchoi <lkamna12@gmail.com>
2019-12-05 23:01:36 +0900

    feat: front 동시 build

--- common/src/frontend/vue.config.js
+++ common/src/frontend/vue.config.js
@@ -1,5 +1,17 @@
+const path = require("path");
+
 module.exports = {
-    "transpileDependencies": [
-        "vuetify"
-    ]
-}
+  "transpileDependencies": [
+    "vuetify"
+  ],
+  outputDir: path.resolve(__dirname, "../../../src/main/resources/static"),
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:9000',
+        ws: true,
+        changeOrigin: true
+      },
+    },
+  },
+};
