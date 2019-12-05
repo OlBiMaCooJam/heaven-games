eb7f663657dcab7ce88872294ced1d1a63c398e5
KIMJUNGMIN <kimjungmin.developer@gmail.com>
2019-12-05 18:11:21 +0900

    refactor: RoomService 구현

--- src/main/java/com/olbimacoojam/heaven/controller/RoomApiController.java
+++ src/main/java/com/olbimacoojam/heaven/controller/RoomApiController.java
@@ -1,45 +1,42 @@
 package com.olbimacoojam.heaven.controller;
 
 import com.olbimacoojam.heaven.domain.Room;
-import com.olbimacoojam.heaven.domain.RoomFactory;
-import com.olbimacoojam.heaven.domain.RoomRepository;
 import com.olbimacoojam.heaven.dto.RoomResponseDto;
-import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.olbimacoojam.heaven.service.RoomService;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequestMapping("/rooms")
 @RestController
 public class RoomApiController {
-    private final RoomRepository roomRepository;
-    private final RoomFactory roomFactory;
-    private final ModelMapper modelMapper;
+//    private final RoomRepository roomRepository;
+//    private final RoomFactory roomFactory;
+    private final RoomService roomService;
+//    private final ModelMapper modelMapper;
 
-    public RoomApiController(RoomRepository roomRepository, RoomFactory roomFactory, ModelMapper modelMapper) {
-        this.roomRepository = roomRepository;
-        this.roomFactory = roomFactory;
-        this.modelMapper = modelMapper;
+    @Autowired
+    public RoomApiController(RoomService roomService) {
+        this.roomService = roomService;
+//        this.roomRepository = roomRepository;
+//        this.roomFactory = roomFactory;
+//        this.modelMapper = modelMapper;
     }
 
     @PostMapping
     public ResponseEntity save() {
-        Room room = roomFactory.makeNextRoom();
-        roomRepository.save(room);
-        return ResponseEntity.created(URI.create("rooms/" + room.getId())).build();
+        RoomResponseDto roomResponseDto = roomService.createRoom();
+        return ResponseEntity.created(URI.create("rooms/" + roomResponseDto.getId())).build();
     }
 
     @GetMapping
     public List<RoomResponseDto> list() {
-        List<Room> rooms = roomRepository.findAll();
-        return rooms.stream()
-                .map(room -> modelMapper.map(room, RoomResponseDto.class))
-                .collect(Collectors.toList());
+        return roomService.findAll();
     }
 }
