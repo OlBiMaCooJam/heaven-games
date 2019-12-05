02cea3053ed8fdf5282fa3ae3fc62b13bdf6d18e
SEOKHYOENCHOI <42906668+SEOKHYOENCHOI@users.noreply.github.com>
2019-12-05 13:30:36 +0900

    reafactor: 리스트 페이지 수정 (#14)
    * style: formatting
    
    * feat: game list 페이지 구현
    
    * feat: 홈 버튼 및 title 추가
    
    * feat: 데이터구조 변경
    
    * feat: 게임 클릭 시 해당 게임으로 이동
    
    * style: 게임이름 오타 수정
    
    * refactor: 홈 버튼 위치 고정
    
    * style: indent 2-2-4로 통일
    
    * style: margin 변경
    
    * style: request api dependency 추가

--- common/src/frontend/src/components/GameThumbnail.vue
+++ common/src/frontend/src/components/GameThumbnail.vue
@@ -1,33 +1,32 @@
 <template>
-    <router-link :to="'/games/' + game.id">
-
-        <v-card
-                class="mx-auto ma-3"
-                max-width="400"
-                tile
-        >
-            <v-img
-                    class="white--text align-end"
-                    height="400px"
-                    :src="game.src"
-            >
-                <v-card-title>{{game.title}}</v-card-title>
-            </v-img>
-        </v-card>
-    </router-link>
+  <router-link :to="'/games/' + game.id">
+    <v-card
+        class="mx-auto ma-3"
+        max-width="400"
+        tile
+    >
+      <v-img
+          :src="game.src"
+          class="white--text align-end"
+          height="400px"
+      >
+        <v-card-title>{{game.title}}</v-card-title>
+      </v-img>
+    </v-card>
+  </router-link>
 </template>
 
 <script>
-    export default {
-        name: "GameThumbnail",
-        props: {
-            game: {
-                title: String,
-                src: String,
-                link: String
-            }
-        }
+  export default {
+    name: "GameThumbnail",
+    props: {
+      game: {
+        title: String,
+        src: String,
+        link: String
+      }
     }
+  }
 </script>
 
 <style scoped>
--- common/src/frontend/src/views/Game.vue
+++ common/src/frontend/src/views/Game.vue
@@ -1,15 +1,13 @@
 <template>
-    <div>
-        <span>Game Id : {{$route.params.id}}</span>
-    </div>
+  <v-container>
+    <span>Game Id : {{$route.params.id}}</span>
+  </v-container>
 </template>
 
 <script>
-    export default {
-        name: "Game"
-    }
-
-
+  export default {
+    name: "Game"
+  }
 </script>
 
 <style scoped>
