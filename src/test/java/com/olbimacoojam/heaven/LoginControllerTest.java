6b24d1aa7e5a485bbabf6020b362b712a1ac38a7
school0bhy <38012258+school0bhy@users.noreply.github.com>
2019-12-09 18:48:23 +0900

    Spring REST Docs 적용 (#27)
    * chore: Spring REST Docs를 위한 의존성 추가
    
    * feat: RoomApi, LoginApi Spring REST docs 적용

--- src/test/java/com/olbimacoojam/heaven/LoginControllerTest.java
+++ src/test/java/com/olbimacoojam/heaven/LoginControllerTest.java
@@ -1,14 +1,32 @@
 package com.olbimacoojam.heaven;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration;
+
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @SpringBootTest(properties = "spring.config.location=classpath:kakao.yml", webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LoginControllerTest {
-    @Autowired
     private WebTestClient webTestClient;
+    @LocalServerPort
+    private String port;
+
+    @BeforeEach
+    void setUp(RestDocumentationContextProvider restDocumentation) {
+        webTestClient = WebTestClient.bindToServer()
+                .baseUrl("http://localhost:" + port)
+                .filter(documentationConfiguration(restDocumentation))
+                .build();
+    }
 
     @Test
     void login() {
@@ -16,6 +34,8 @@ class LoginControllerTest {
                 .get()
                 .uri("/login/kakao")
                 .exchange()
-                .expectStatus().isFound();
+                .expectStatus().isFound()
+                .expectBody()
+                .consumeWith(document("/login-api/login"));
     }
 }
