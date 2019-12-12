653ac8a65abd441a7891732100297792c6c3b790
hyogeon <men1219@hanmail.net>
2019-12-12 10:17:01 +0900

    feat: MafiaGame 구현

--- src/main/java/com/olbimacoojam/heaven/mafia/MafiaGame.java
+++ src/main/java/com/olbimacoojam/heaven/mafia/MafiaGame.java
@@ -2,6 +2,7 @@ package com.olbimacoojam.heaven.mafia;
 
 import com.olbimacoojam.heaven.domain.User;
 import com.olbimacoojam.heaven.game.Game;
+import com.olbimacoojam.heaven.mafia.Initializer.MafiaGameParticipantsNumberRange;
 
 import java.util.List;
 
@@ -13,17 +14,9 @@ public class MafiaGame implements Game {
      */
     @Override
     public void initialize(List<User> players) {
-        //최소 4명
-        //4~5명은 마피아 1명, 6명 이상 2명, 9명 이상 3명 - 최대 10명 제한
-        //특수 직업, 마피아 랜덤 배정
-        //4~5명 이면, 마피아 1, 시민 2~3, 의사1
-        //6~8명 이면, 마피아 2, 시민 2~4, 의사1, 경찰1
-        //9~10명 이면, 마피아 3, 시민 2~3, 의사1, 경찰1, 군인1, 사립탐정1
+        List<MafiaParticipant> mafiaParticipants = MafiaGameParticipantsNumberRange.initialize(players);
     }
 
-    /**
-     *
-     */
     public boolean isEnd() {
         return true;
     }
