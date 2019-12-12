9f1eab31a98a03af7853cc3f15ea557d628c3328
seokhyeonchoi <lkamna12@gmail.com>
2019-12-12 12:54:14 +0900

    feat: 생성자 접근 제어 private으로 변경

--- src/main/java/com/olbimacoojam/heaven/minesweeper/domain/Board.java
+++ src/main/java/com/olbimacoojam/heaven/minesweeper/domain/Board.java
@@ -5,7 +5,7 @@ import java.util.Map;
 public class Board {
     private final Map<Position, Block> board;
 
-    public Board(final Map<Position, Block> board) {
+    private Board(final Map<Position, Block> board) {
         this.board = board;
     }
 
