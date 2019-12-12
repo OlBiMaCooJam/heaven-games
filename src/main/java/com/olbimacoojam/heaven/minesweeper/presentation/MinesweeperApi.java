89c9438621b5f5a289ccedc252c159a89c73b08c
seokhyeonchoi <lkamna12@gmail.com>
2019-12-12 13:07:45 +0900

    refactor: Return type ResponseEntity로 변경 및 URL 변경

--- src/main/java/com/olbimacoojam/heaven/minesweeper/presentation/MinesweeperApi.java
+++ src/main/java/com/olbimacoojam/heaven/minesweeper/presentation/MinesweeperApi.java
@@ -8,24 +8,27 @@ import com.olbimacoojam.heaven.minesweeper.application.MinesweeperService;
 import com.olbimacoojam.heaven.minesweeper.domain.Board;
 import com.olbimacoojam.heaven.service.RoomService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 
 @RestController
-@RequestMapping("/minesweeper")
+@RequestMapping("/rooms/{roomId}/minesweeper")
 @RequiredArgsConstructor
 public class MinesweeperApi {
     private final MinesweeperService minesweeperService;
     private final RoomService roomService;
 
-    @GetMapping("/rooms/{roomId}/new")
-    public Board newGame(HttpSession httpSession, @PathVariable Integer roomId, MinesweeperCreateRequest request) {
+    @PostMapping
+    public ResponseEntity newGame(HttpSession httpSession, @PathVariable Integer roomId, MinesweeperCreateRequest request) {
         User user = (User) httpSession.getAttribute(UserSession.USER_SESSION);
         Room room = roomService.findById(roomId);
-        return minesweeperService.createGame(room.getPlayers(), user, request);
+        Board board = minesweeperService.createGame(room.getPlayers(), user, request);
+
+        return ResponseEntity.ok(board);
     }
 }
