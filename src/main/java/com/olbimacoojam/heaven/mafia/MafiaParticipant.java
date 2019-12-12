06e01baadcd557af5764831e0c205b6d09a1d5f1
hyogeon <men1219@hanmail.net>
2019-12-12 10:17:19 +0900

    feat: MafiaParcipant 구현

--- src/main/java/com/olbimacoojam/heaven/mafia/MafiaParticipant.java
+++ src/main/java/com/olbimacoojam/heaven/mafia/MafiaParticipant.java
@@ -1,8 +1,16 @@
 package com.olbimacoojam.heaven.mafia;
 
 import com.olbimacoojam.heaven.domain.User;
+import com.olbimacoojam.heaven.mafia.Occupation.Occupation;
+import lombok.Getter;
 
+@Getter
 public class MafiaParticipant {
     private User player;
     private Occupation occupation;
+
+    public MafiaParticipant(User player, Occupation occupation) {
+        this.player = player;
+        this.occupation = occupation;
+    }
 }
