96d76983b8b89ec617fc2189caf6fbf75dd72ab7
seokhyeonchoi <lkamna12@gmail.com>
2019-12-13 01:30:09 +0900

    refactor: 추후 구현

--- src/main/java/com/olbimacoojam/heaven/minesweeper/domain/MinesweeperRepository.java
+++ src/main/java/com/olbimacoojam/heaven/minesweeper/domain/MinesweeperRepository.java
@@ -1,8 +1,4 @@
 package com.olbimacoojam.heaven.minesweeper.domain;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MinesweeperRepository extends JpaRepository<Board, Long> {
+public interface MinesweeperRepository {
 }
