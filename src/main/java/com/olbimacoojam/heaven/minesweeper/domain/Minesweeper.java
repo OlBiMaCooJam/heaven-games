a824cc18f71f1898d24d5bb3f4260a1b419c07ac
seokhyeonchoi <lkamna12@gmail.com>
2019-12-12 10:03:19 +0900

    feat: 지뢰찾기 메인 클래스 추가

---
+++ src/main/java/com/olbimacoojam/heaven/minesweeper/domain/Minesweeper.java
@@ -0,0 +1,49 @@
+package com.olbimacoojam.heaven.minesweeper.domain;
+
+import com.olbimacoojam.heaven.domain.User;
+import com.olbimacoojam.heaven.game.Game;
+import com.olbimacoojam.heaven.minesweeper.domain.exception.InvalidNumberOfUserException;
+import com.olbimacoojam.heaven.minesweeper.domain.exception.UserNotMatchException;
+
+import java.util.List;
+
+public class Minesweeper implements Game {
+    private static final int MINESWEEPER_PLAYER_NUMBER = 1;
+    private final Board board;
+    private final User user;
+
+    private Minesweeper(final User user, final Board board) {
+        this.board = board;
+        this.user = user;
+    }
+
+    public static Minesweeper newGame(final User user, final Board board) {
+        return new Minesweeper(user, board);
+    }
+
+    @Override
+    public void initialize(final List<User> players) {
+        checkUser(players);
+    }
+
+    private void checkUser(List<User> players) {
+        checkNumberOfPlayers(players);
+        checkSameUser(players);
+    }
+
+    private void checkNumberOfPlayers(List<User> players) {
+        if (players.size() != MINESWEEPER_PLAYER_NUMBER) {
+            throw new InvalidNumberOfUserException(MINESWEEPER_PLAYER_NUMBER, players.size());
+        }
+    }
+
+    private void checkSameUser(List<User> players) {
+        if (players.contains(user)) {
+            throw new UserNotMatchException();
+        }
+    }
+
+    public Board getBoard() {
+        return this.board;
+    }
+}
---
+++ src/main/java/com/olbimacoojam/heaven/minesweeper/domain/exception/InvalidNumberOfUserException.java
@@ -0,0 +1,12 @@
+package com.olbimacoojam.heaven.minesweeper.domain.exception;
+
+public class InvalidNumberOfUserException extends RuntimeException {
+
+    public InvalidNumberOfUserException(Integer expected, Integer actual) {
+        super(buildMessage("잘못된 플레이어 수 입니다. expected : %d, actual : %d", expected, actual));
+    }
+
+    private static String buildMessage(String message, Integer expected, Integer actual) {
+        return String.format(message, expected, actual);
+    }
+}
