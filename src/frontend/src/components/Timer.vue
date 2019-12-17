0b80a831c6524f4b28647ea22e63911cf0d5a613
Seongmo Jeon <oeeen3@gmail.com>
2019-12-17 12:54:30 +0900

    feat: 타이머 프론트 구현

---
+++ src/frontend/src/components/Timer.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <div class="block">
+            <p class="digit">{{ seconds | two_digits }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Timer",
+        mounted: function () {
+            window.setInterval(() => {
+                this.now = Math.trunc((new Date()).getTime() / 1000);
+            }, 1000);
+        },
+        props: {
+            date: Number
+        },
+
+        data() {
+            return {
+                now: Math.trunc((new Date()).getTime() / 1000),
+                limit: Math.trunc((new Date().getTime() / 1000) + Number(this.date)),
+            }
+        },
+
+        computed: {
+            seconds() {
+                const time = this.limit - this.now;
+                return time > 0 ? time : 0;
+            },
+        },
+    }
+
+</script>
+
+<style scoped>
+    @import url(https://fonts.googleapis.com/css?family=Roboto+Condensed:400|Roboto:100);
+
+    .block {
+        display: flex;
+        flex-direction: column;
+        margin: 20px;
+    }
+
+    .digit {
+        color: #ecf0f1;
+        font-size: 80px;
+        font-weight: 100;
+        font-family: 'Roboto', serif;
+        margin: 10px;
+        text-align: center;
+    }
+</style>
--- src/frontend/src/views/Mafia.vue
+++ src/frontend/src/views/Mafia.vue
@@ -6,16 +6,23 @@
                     <MafiaCitizen :citizen="citizen" :key="citizen.name" v-for='citizen in citizens'/>
                 </v-row>
             </v-col>
+            <v-col cols="6">
+                <v-row align='center' justify='center'>
+                    <Timer :date=date></Timer>
+                </v-row>
+            </v-col>
         </v-row>
     </v-container>
 </template>
 
 <script>
     import MafiaCitizen from "../components/MafiaCitizen";
+    import Timer from "../components/Timer";
 
     export default {
         name: 'Mafia',
         components: {
+            Timer,
             MafiaCitizen
         },
         data() {
@@ -30,7 +37,8 @@
                     {name: '맥코맥', occupation: '마피아', src: require('../assets/p8.png')},
                     {name: '쿠기쿠', occupation: '마피아', src: require('../assets/p9.png')},
                     {name: '모비', occupation: '마피아', src: require('../assets/p10.png')}, // 테스트 데이터
-                ]
+                ],
+                date: 234, // 테스트 데이터
             }
         },
         id: 1,
