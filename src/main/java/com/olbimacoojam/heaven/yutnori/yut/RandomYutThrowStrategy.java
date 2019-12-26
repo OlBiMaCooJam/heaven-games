e445b303fdae6db1727c1f67ef9e17f13a9cd18b
KIMJUNGMIN <kimjungmin.developer@gmail.com>
2019-12-26 11:54:14 +0900

    refactor: RandomYutThrowStrategy 확률 조정

--- src/main/java/com/olbimacoojam/heaven/yutnori/yut/RandomYutThrowStrategy.java
+++ src/main/java/com/olbimacoojam/heaven/yutnori/yut/RandomYutThrowStrategy.java
@@ -1,15 +1,36 @@
 package com.olbimacoojam.heaven.yutnori.yut;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class RandomYutThrowStrategy implements YutThrowStrategy {
+    private List<Integer> probabilities;
+
+    public RandomYutThrowStrategy() {
+        this.probabilities = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17));
+    }
 
     @Override
     public Yut throwYut() {
-        List<Yut> yuts = Arrays.asList(Yut.values());
-        Collections.shuffle(yuts);
-        return yuts.get(2);
+        Collections.shuffle(probabilities);
+        int probability = probabilities.get(0);
+        if (probability >= 1 && probability <= 4) {
+            return Yut.DO;
+        }
+        if (probability >= 5 && probability <= 10) {
+            return Yut.GAE;
+        }
+        if (probability >= 11 && probability <= 14) {
+            return Yut.GUL;
+        }
+        if (probability == 15) {
+            return Yut.YUT;
+        }
+        if (probability == 16) {
+            return Yut.MO;
+        }
+        return Yut.BACKDO;
     }
 }
