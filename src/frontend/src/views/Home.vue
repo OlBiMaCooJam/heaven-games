14b2f2565630def8074838a94d15853edf5993b6
Seongmo Jeon <oeeen3@gmail.com>
2019-12-26 11:38:57 +0900

    feat: 제비뽑기 썸네일 추가

--- src/frontend/src/views/Home.vue
+++ src/frontend/src/views/Home.vue
@@ -11,9 +11,9 @@
 </template>
 
 <script>
-  import GameThumbnail from "../components/GameThumbnail";
+    import GameThumbnail from "../components/GameThumbnail";
 
-  export default {
+    export default {
     name: 'home',
     components: {
       GameThumbnail
@@ -23,9 +23,9 @@
         games: [{title: '마피아게임', src: require('../assets/mafia_thumbnail.jpg'), gameTitle: 'MAFIA'},
           {title: '윷놀이', src: require('../assets/yutnori_thumbnail.jpeg'), gameTitle: 'YUTNORI'},
           {title: '지뢰찾기', src: require('../assets/minesweeper_thumbnail.jpeg'), gameTitle: 'MINE'},
+          {title: '제비뽑기', src: require('../assets/draw_thumbnail.png'), gameTitle: 'DRAW'},
           {title: '사다리타기', src: require('../assets/ladder_thumbnail.png'), gameTitle: 'LADDER'},
-          {title: '노래맞추기', src: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg', gameTitle: 'SONG'},
-          {title: '제비뽑기', src: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg', gameTitle: 'DRAW'}],
+          {title: '노래맞추기', src: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg', gameTitle: 'SONG'}],
       }
     }
   }
