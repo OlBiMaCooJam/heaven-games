d0e40df34c72d9092036eb8255a7c2017a2868ce
school0bhy <house1217@ajou.ac.kr>
2019-12-26 20:23:24 +0900

    feat: 도개걸윷모 확률 로직 변경

--- src/main/java/com/olbimacoojam/heaven/yutnori/yut/Yut.java
+++ src/main/java/com/olbimacoojam/heaven/yutnori/yut/Yut.java
@@ -4,17 +4,19 @@ import com.olbimacoojam.heaven.yutnori.piece.moveresult.Route;
 
 public enum Yut {
 
-    DO(1),
-    GAE(2),
-    GUL(3),
-    YUT(4),
-    MO(5),
-    BACKDO(-1);
+    DO(1, 8),
+    GAE(2, 12),
+    GUL(3, 8),
+    YUT(4, 3),
+    MO(5, 2),
+    BACKDO(-1, 1);
 
     private final int moveUnit;
+    private final int weight;
 
-    Yut(int moveUnit) {
+    Yut(int moveUnit, int weight) {
         this.moveUnit = moveUnit;
+        this.weight = weight;
     }
 
     public static Yut get(String pointName) {
@@ -27,5 +29,9 @@ public enum Yut {
 
     public boolean canMove(Route route) {
         return route.size() <= moveUnit;
+    }
+
+    public int getWeight() {
+        return weight;
     }
 }
