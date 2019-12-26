f0e39516f8e578cf3aa619ac3691bcced3bb198c
Seongmo Jeon <oeeen3@gmail.com>
2019-12-26 21:05:45 +0900

    test: deploy test

--- src/test/java/com/olbimacoojam/heaven/mafia/Client.java
+++ src/test/java/com/olbimacoojam/heaven/mafia/Client.java
@@ -1,12 +1,15 @@
 package com.olbimacoojam.heaven.mafia;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.stomp.StompSession;
 
 @Getter
-@RequiredArgsConstructor
 public class Client {
     private final StompSession stompSession;
     private final String jsessionId;
+
+    public Client(StompSession stompSession, String jsessionId) {
+        this.stompSession = stompSession;
+        this.jsessionId = jsessionId;
+    }
 }
