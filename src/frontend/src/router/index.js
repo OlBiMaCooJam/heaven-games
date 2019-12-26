c050469dc82c62b5c043bb1402b1ddcc53d0974c
Seongmo Jeon <oeeen3@gmail.com>
2019-12-26 15:56:07 +0900

    style: change indent

--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -4,43 +4,43 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: () => import('../views/Home.vue')
-  },
-  {
-      path: '/:gameTitle/rooms/:id',
-    name: 'game',
-      component: () => import('../views/Game.vue'),
-      props: true,
-  },
-  {
-      path: '/:gameTitle/rooms',
-    name: 'RoomList',
-    component: () => import('../views/RoomList.vue')
-      props: true,
-  },
-  {
-    path: '/rooms/:id/yutnori',
-    name: 'Yutnori',
-    component: () => import('../views/Yutnori.vue')
-  },
-  // {
-  //   path: '/rooms/:id',
-  //   name: 'game',
-  //   component: () => import('../views/Game.vue')
-  // },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('../views/Login.vue')
-  },
-  {
-    path: '/minesweeper',
-    name: 'minesweeper',
-    component: () => import('../views/Minesweeper.vue')
-  },
+    {
+        path: '/',
+        name: 'home',
+        component: () => import('../views/Home.vue')
+    },
+    {
+        path: '/:gameTitle/rooms/:id',
+        name: 'game',
+        component: () => import('../views/Game.vue'),
+        props: true,
+    },
+    {
+        path: '/:gameTitle/rooms',
+        name: 'RoomList',
+        component: () => import('../views/RoomList.vue'),
+        props: true,
+    },
+    {
+        path: '/rooms/:id/yutnori',
+        name: 'Yutnori',
+        component: () => import('../views/Yutnori.vue')
+    },
+    // {
+    //   path: '/rooms/:id',
+    //   name: 'game',
+    //   component: () => import('../views/Game.vue')
+    // },
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('../views/Login.vue')
+    },
+    {
+        path: '/minesweeper',
+        name: 'minesweeper',
+        component: () => import('../views/Minesweeper.vue')
+    },
     {
         path: '/MAFIA/rooms/:id/start',
         name: 'mafia',
@@ -55,9 +55,9 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 });
 
 export default router
