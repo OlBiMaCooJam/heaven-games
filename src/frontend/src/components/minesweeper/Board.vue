67db5baed82d255eb915ef15e81cca4d0c7261cd
seokhyeonchoi <lkamna12@gmail.com>
2019-12-27 11:54:58 +0900

    fix: bug fix

--- src/frontend/src/components/minesweeper/Board.vue
+++ src/frontend/src/components/minesweeper/Board.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container id="board">
-    <template v-for="y in blocks.length">
+    <template v-for="y in boardBlocks.length">
       <v-row no-gutters :key="y" justify="center">
-        <template v-for="x in blocks[0].length">
+        <template v-for="x in boardBlocks[0].length">
           <v-col :key="x" style="flex-grow: 0">
-            <Block @clickBlock="clickBlock" :x="x" :y="y" :block="blocks[y-1][x-1]"></Block>
+            <Block @clickBlock="clickBlock" :x="x" :y="y" :block="boardBlocks[y-1][x-1]"></Block>
           </v-col>
         </template>
       </v-row>
@@ -13,7 +13,6 @@
 </template>
 
 <script>
-  import axios from 'axios';
   import Block from "./Block";
 
   export default {
@@ -22,95 +21,11 @@
       Block
     },
     props: {
-      boardBlocks: Array,
-    },
-    data() {
-      return {
-        blocks: this.boardBlocks,
-        minesweeperStatus: '',
-      }
+      boardBlocks: Array
     },
     methods: {
       clickBlock(x, y, clickType) {
-        if (this.checkGameOver()) {
-          return;
-        }
-
-        if ('LEFT' === clickType) {
-          this.clickLeft(x, y);
-          return;
-        }
-        this.clickRight(x, y);
-      },
-      async clickLeft(x, y) {
-        const clickResponse = await this.click(x, y, "LEFT");
-        const clickedBlocks = clickResponse.clickedBlocks;
-        this.minesweeperStatus = clickResponse.minesweeperStatus;
-        for (let i = 0, len = clickedBlocks.length; i < len; i++) {
-          const clickedBlock = clickedBlocks[i];
-          this.blocks = this.changeStatus(clickedBlock.x, clickedBlock.y,
-              clickedBlock.blockStatus, clickedBlock.numberOfAroundMines);
-        }
-
-        this.checkGameOver();
-      },
-      async clickRight(x, y) {
-        const clickResponse = await this.click(x, y, "RIGHT");
-        const clickedBlocks = clickResponse.clickedBlocks;
-        this.blocks = this.changeStatus(x, y, clickedBlocks[0].blockStatus, clickedBlocks[0].numberOfAroundMines);
-      },
-      changeStatus(x, y, blockStatus, numberOfAroundMines) {
-        const nextStatus = {
-          iconName: blockStatus,
-          numberOfAroundMines: numberOfAroundMines
-        };
-        let newBoard = [...this.blocks];
-        newBoard[y].splice(x, 1, nextStatus);
-        return newBoard;
-      },
-      async click(x, y, clickType) {
-        try {
-          const response = await axios.put('/rooms/1/minesweeper', {
-            position: {
-              x: x,
-              y: y
-            },
-            clickType: clickType
-          });
-          return {
-            clickedBlocks: this.parseBlocks(response.data.clickedBlocks.clickedBlocks),
-            minesweeperStatus: response.data.minesweeperStatus
-          };
-        } catch (error) {
-          alert(error)
-        }
-      },
-      checkGameOver() {
-        if (this.isGameOver()) {
-          alert(this.minesweeperStatus);
-          return true;
-        }
-        return false;
-      },
-      isGameOver() {
-        return "WIN" === this.minesweeperStatus || "LOSE" === this.minesweeperStatus;
-      },
-      parseBlocks(clickedBlocks) {
-        const blocks = [];
-        for (let block in clickedBlocks) {
-          const position = this.parsePosition(block);
-          clickedBlocks[block].x = position.x;
-          clickedBlocks[block].y = position.y;
-          blocks.push(clickedBlocks[block])
-        }
-
-        return blocks;
-      },
-      parsePosition(block) {
-        return {
-          x: block.match("x=[0-9]+")[0].split("=")[1],
-          y: block.match("y=[0-9]+")[0].split("=")[1]
-        }
+        this.$emit("clickBlock", x, y, clickType);
       },
     }
   }
--- src/frontend/src/views/Minesweeper.vue
+++ src/frontend/src/views/Minesweeper.vue
@@ -1,12 +1,12 @@
 <template>
   <v-row justify="center">
     <BoardSpec :dialog="dialog" @createGame="createGame"></BoardSpec>
-    <Board v-if="gameCreated" :boardBlocks="blocks" :gameCreated="gameCreated"></Board>
+    <Board v-if="gameCreated" @clickBlock="clickBlock" :boardBlocks="blocks"
+           :minesweeperStatus="minesweeperStatus"></Board>
   </v-row>
 </template>
 <script>
   import axios from 'axios';
-
   import Board from '../components/minesweeper/Board'
   import BoardSpec from "../components/minesweeper/BoardSpec";
 
@@ -22,6 +22,7 @@
         dialog: true,
         gameCreated: false,
         roomId: this.$route.params.id,
+        minesweeperStatus: "PLAYING"
       }
     },
     methods: {
@@ -54,6 +55,87 @@
             };
             this.blocks[i].push(block);
           }
+        }
+      },
+      clickBlock(x, y, clickType) {
+        if (this.checkGameOver()) {
+          return;
+        }
+
+        if ('LEFT' === clickType) {
+          this.clickLeft(x, y);
+          return;
+        }
+        this.clickRight(x, y);
+      },
+      async clickLeft(x, y) {
+        const clickResponse = await this.click(x, y, "LEFT");
+        const clickedBlocks = clickResponse.clickedBlocks;
+        this.minesweeperStatus = clickResponse.minesweeperStatus;
+        for (let i = 0, len = clickedBlocks.length; i < len; i++) {
+          const clickedBlock = clickedBlocks[i];
+          this.blocks = this.changeStatus(clickedBlock.x, clickedBlock.y,
+              clickedBlock.blockStatus, clickedBlock.numberOfAroundMines);
+        }
+
+        this.checkGameOver();
+      },
+      async clickRight(x, y) {
+        const clickResponse = await this.click(x, y, "RIGHT");
+        const clickedBlocks = clickResponse.clickedBlocks;
+        this.blocks = this.changeStatus(x, y, clickedBlocks[0].blockStatus, clickedBlocks[0].numberOfAroundMines);
+      },
+      changeStatus(x, y, blockStatus, numberOfAroundMines) {
+        const nextStatus = {
+          iconName: blockStatus,
+          numberOfAroundMines: numberOfAroundMines
+        };
+        let newBoard = [...this.blocks];
+        newBoard[y].splice(x, 1, nextStatus);
+        return newBoard;
+      },
+      async click(x, y, clickType) {
+        try {
+          const response = await axios.put(`/rooms/${this.roomId}/minesweeper`, {
+            position: {
+              x: x,
+              y: y
+            },
+            clickType: clickType
+          });
+          return {
+            clickedBlocks: this.parseBlocks(response.data.clickedBlocks.clickedBlocks),
+            minesweeperStatus: response.data.minesweeperStatus
+          };
+        } catch (error) {
+          alert(error)
+        }
+      },
+      checkGameOver() {
+        if (this.isGameOver()) {
+          alert(this.minesweeperStatus);
+          return true;
+        }
+        return false;
+      },
+      isGameOver() {
+        return "WIN" === this.minesweeperStatus || "LOSE" === this.minesweeperStatus;
+      },
+      parseBlocks(clickedBlocks) {
+        const blocks = [];
+        for (let block in clickedBlocks) {
+          const position = this.parsePosition(block);
+          clickedBlocks[block].x = position.x;
+          clickedBlocks[block].y = position.y;
+          blocks.push(clickedBlocks[block])
+        }
+
+        return blocks;
+      },
+      parsePosition(block) {
+        return {
+          x: block.match("x=[0-9]+")[0].split("=")[1],
+          y: block.match("y=[0-9]+")[0].split("=")[1]
         }
       },
     }
